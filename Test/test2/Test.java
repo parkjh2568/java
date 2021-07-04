200c4623e288bdf864691c2ee1f1a6759ed1a95d
junhypar <junhypar@student.42seoul.kr>
2021-07-04 10:26:30 +0900

    wow

---
+++ Test/test2/Test.java
@@ -0,0 +1,14 @@
+class Test{
+	int a;
+	String[] b = new String[]{"hi","im","ur","fathr"
+	};
+
+	public static void main(String argc[]){
+		System.out.println("java는 기본적으로 System.in, Ststem.out, System.err를 통해 사용한다");
+		System.out.print("System.out.print는 개행이없는 프린트\n");
+		System.out.println("문자, 숫자 여러 출력물은 '+' 를통해 이어서 " + "출력이 가능하다" + 3 + 9 "예스");
+		System.out.
+	};
+
+
+}
